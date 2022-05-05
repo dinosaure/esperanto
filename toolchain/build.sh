5b9dcdde18685ebfca8e5c92fc370174b98a7d8c
Romain Calascibetta <romain.calascibetta@gmail.com>
2022-05-05 16:31:47 +0200

    Fix the usage of some flags and bootstrap the project with extract.com

--- toolchain/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ocamlfind opt -linkpkg -package digestif.c,digestif,fmt,camlzip \
-	extract.ml -o extract.com
-rm extract.cmi
-rm extract.cmx
-rm extract.o
--- toolchain/startup.c
+++ toolchain/startup.c
@@ -1,10 +1,31 @@
+#include "cosmopolitan/cosmopolitan.h"
+
 #pragma GCC push
 #pragma GCC diagnostic ignored "-Wimplicit-function-declaration"
 
-int
-main(int argc, char *argv[]) {
-  caml_startup(argv);
-  // TODO(dinosaure): check if [argv] terminates with [NULL].
+/* XXX(dinosaure): we tweak the caml runtime which should provide these
+ * functions. The goal is to reset [sys_open_flags]/[posix_signals] by right
+ * values given by Cosmopolitan **after** the APE boot. */
+extern void _esperanto_init_sys_open_flags(const int[]);
+extern void _esperanto_init_posix_signals(const int[]);
+
+int main(int argc, char *argv[]) {
+  int open_flags[] = {
+      O_RDONLY, O_WRONLY, O_APPEND | O_WRONLY, O_CREAT, O_TRUNC, O_EXCL,
+      0,        0,        O_NONBLOCK}; // Cosmopolitan does not provide O_BINARY
+                                       // and O_TRUNC
+  int posix_signals[] = {SIGABRT, SIGALRM,   SIGFPE,  SIGHUP,  SIGILL,  SIGINT,
+                         SIGKILL, SIGPIPE,   SIGQUIT, SIGSEGV, SIGTERM, SIGUSR1,
+                         SIGUSR2, SIGCHLD,   SIGCONT, SIGSTOP, SIGTSTP, SIGTTIN,
+                         SIGTTOU, SIGVTALRM, SIGPROF, SIGBUS,  SIGPOLL, SIGSYS,
+                         SIGTRAP, SIGURG,    SIGXCPU, SIGXFSZ};
+
+  _esperanto_init_sys_open_flags(open_flags);
+  _esperanto_init_posix_signals(posix_signals);
+
+  caml_startup(
+      argv); // TODO(dinosaure): check if [argv] terminates with [NULL].
+
   return (0);
 }
 
